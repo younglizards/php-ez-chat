fb2306ca739984d437368269e8f7a6e19156bea0
M4lek <aloro2392@gmail.com>
2018-07-07 00:22:12 +0200

    Server initialised.

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Servidor</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/style.css" />
+</head>
+<body>
+<?php 
+
+$host = "localhost";
+$port = "13811";
+
+echo "Servidor alojado en " . $host . " en el puerto " . $port;
+
+
+?>
+</body>
+</html>
