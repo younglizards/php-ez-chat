cf13c6801fcdf7a050a34ddabaab761c63ee6f13
M4lek <aloro2392@gmail.com>
2018-07-07 01:45:36 +0200

    Server connection

--- index.php
+++ index.php
@@ -1,21 +1,67 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Servidor</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="css/style.css" />
-</head>
-<body>
-<?php 
+<?php
+
+error_reporting(E_ALL);
+
+set_time_limit(0);
+
+ob_implicit_flush();
+
 
 $host = "localhost";
-$port = "13811";
+$port = "13813";
+$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+
+define("SOCKET_LAST_ERROR", "\"" . socket_strerror(socket_last_error($socket)) . "\"\n");
+
+if($socket === false) {
+    echo "La creación del socket ha fallado debido a " . SOCKET_LAST_ERROR;
+}
+
+if(socket_bind($socket, $host, $port) === false) {
+    echo "El bindeo del socket ha fallado debido a " .  SOCKET_LAST_ERROR;
+}
+
+if(socket_listen($socket, 5) === false) {
+    echo "El listen en el socket ha fallado debido a " . SOCKET_LAST_ERROR;
+}
+
+do {
+    if(($msgSocket = socket_accept($socket)) === false) {
+        echo "El socket_accept ha fallado por " . SOCKET_LAST_ERROR;
+        break;
+
+        $msg = "Bienvenido al servidor de Da Homa.\n Para salir del servidor de Da Homa puedes escribir 'quit'.\n Para cerrar el servidor puedes escribir 'shutdown'\n";
+        socket_write($msgSocket, $msg, strlen($msg));
+    }
+    do {
+
+        if(($buf = socket_read($msgSocket, 2048, PHP_NORMAL_READ)) === false) {
+            echo "El socket_read ha fallado por " . SOCKET_LAST_ERROR;
+            break 2;
+        }
+
+        if(!$buf = trim($buf)) {
+            continue;
+        }
+
+        if($buf == 'quit') {
+            break;
+        }
+
+        if($buf == 'shutdown') {
+            socket_close($msgSocket);
+            break 2;
+        }
+
+        $m = "[" . date('H:i') . "] Usuario dijo: " . $buf . ".\n";
+        socket_write($msgSocket, $m, strlen($m));
+        echo "Mensaje escrito: " . $buf . "\n";
+
+    } while (true);
+    socket_close();
+} while (true);
+socket_close($socket);
 
 echo "Servidor alojado en " . $host . " en el puerto " . $port . ", waiting for connection.";
 
-
 ?>
-</body>
-</html>
